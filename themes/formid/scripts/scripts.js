b04e155f799ca0bf94d19418a7dbba8dfbdd160c
Steen Larsen <sgl@aarhus.dk>
2012-01-31 15:31:04 +0100

    scripts updated

--- themes/formid/scripts/scripts.js
+++ themes/formid/scripts/scripts.js
@@ -1,20 +1,16 @@
 (function ($) {
 
-  function OpenWinExe(url,title,variables) {
-    popupWin = window.open(url,title,variables);
-    if ( popupWin ) popupWin.focus();
-    return popupWin;
+  function open_window(url,title,h,w,x,y) {
+    var win = window.open(url,title,'scrollbars=1,resizable=yes,height=' + h + ',width=' + w +',left=' + x + ',top=' + y + '');
+    if ( win ) win.focus();
+    return win;
   }
 
-  function PopWinxy(url,title,h,w,x,y) {
-    return OpenWinExe(url,title,'scrollbars=1,resizable=yes,height=' + h + ',width=' + w +',left=' + x + ',top=' + y + '');
-  }
-
-  function Show(keyno, listno){
+  function show_data(keyno, listno){
      if ( listno == '64' ) {
-       PopWinxy('http://netpunkt.dk/pop_vis.php?database=Danbib&format=A&ignorebrowser=1&ccl=id%3D'+keyno, 'lektor',690,785,1,1);
+       open_window('http://netpunkt.dk/pop_vis.php?database=Danbib&format=A&ignorebrowser=1&ccl=id%3D'+keyno, 'lektor',690,785,1,1);
      } else {
-       PopWinxy('http://netpunkt.dk/pop_vis.php?target=Lektor&format=pop_up&header=Lekt%F8rudtalelse&ccl=ld%3d('+keyno+')','lektor',690,585,1,1);
+       open_window('http://netpunkt.dk/pop_vis.php?target=Lektor&format=pop_up&header=Lekt%F8rudtalelse&ccl=ld%3d('+keyno+')','lektor',690,585,1,1);
      }
   }
 
@@ -25,7 +21,7 @@
      $('.netpunkt').click( function() {
         var vars=$(this).attr('rel').split('-')
         if ( vars ) {
-          Show(vars[0], vars[1]);
+          show_data(vars[0], vars[1]);
         }
         return false;
       }
